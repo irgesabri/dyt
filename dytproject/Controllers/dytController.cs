7d67a123faf501de77a9ae2394a171bb6c006ac7
LAPTOP-C5O2ATD6\sirge <engineersabriirge@gmail.com>
2018-07-12 20:51:54 +0300

    deneme

---
+++ dytproject/Controllers/dytController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dytproject.Controllers
+{
+    public class dytController : Controller
+    {
+        // GET: dyt
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
